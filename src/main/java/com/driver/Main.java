a89238b912d59c2622824d8913989b34a9fe56b9
Yogesh Psndey <103723595+yogeshpandey143@users.noreply.github.com>
2022-12-10 12:12:31 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,28 @@ package com.driver;
 
 public class Main {
   
+	public static void main(String[] args) {
+		 
+		B obj = new B();
+		
+		System.out.print(obj.meth());
+	}
+}
+//hghh
+public class A {
+	
+	public String meth()
+	{
+		return "Invoking method from class A";
+	}
+
+}
+public class B extends A{
+	
+	public String meth()
+	{
+		return "Method is overridden in Extendend class B";
+		
+	}
+
 }
